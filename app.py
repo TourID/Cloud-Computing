7dfe21ed7524e25fbd4be5e3e9d0774946de87a3
roihanfs <41983891+Roihan1303@users.noreply.github.com>
2024-06-15 07:31:51 +0700

    Ignore endpoint Model

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from routes.model import model_bp
+# from routes.model import model_bp
 from routes.places import places_bp
 from routes.reviews import reviews_bp
 from routes.users import users_bp
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 # Register blueprints
-app.register_blueprint(model_bp)
+# app.register_blueprint(model_bp)
 app.register_blueprint(places_bp)
 app.register_blueprint(reviews_bp)
 app.register_blueprint(users_bp)
